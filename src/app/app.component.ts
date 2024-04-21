41a90a8394af71a8c0cad7cf6a51fca3e1f5effd
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2024-04-21 12:49:34 -0400

    chore: add tauri

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { invoke } from '@tauri-apps/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tauri-auto-linker';
+
+  constructor() {
+    invoke('greet', { name: "World" })
+      .then((res) => console.log(res))
+  }
 }
